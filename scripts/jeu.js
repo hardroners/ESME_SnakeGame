81c63c30e25a6136f6d328ba1068cb8091fc436a
hardroners <hardroners@gmail.com>
2022-03-20 17:08:55 +0100

    laser can be lauch step by step now

--- scripts/jeu.js
+++ scripts/jeu.js
@@ -2,6 +2,7 @@
 function Sprite(filename, abs,ord,size){
 	this._node = document.createElement("img"); // les annontations _node sont des variables privées ou locales
 	this._node.src = filename;
+	this._node.id = filename;
 	this._node.style.position="absolute";
 	this._node.style.height=size + "px";
 	this._node.style.width="auto";
@@ -55,34 +56,34 @@ let alien1 = new Sprite("images/invader.png",3*document.body.clientWidth/8, docu
 let alien2 = new Sprite("images/invader.png",4*document.body.clientWidth/8, document.body.clientHeight/4,100);
 let alien3 = new Sprite("images/invader.png",5*document.body.clientWidth/8, document.body.clientHeight/4,100);
 let vaisseau = new Sprite("images/vaisseau.png", document.body.clientWidth/2, 3*document.body.clientHeight/4,100);
-let missile = new Sprite("images/laser.png", vaisseau.left,vaisseau.top,50);
-missile.display = "none";
+// let missile = new Sprite("images/laser.png", vaisseau.left,vaisseau.top,50);
+// missile.display = "none";
+let i = 0;
 
 var id = null;
-function myMove(missile) {
+function myMove() {
+	let missile = new Sprite("images/laser.png", vaisseau.left,vaisseau.top,50);
 	missile.display = "none";
 	missile.left = vaisseau.left;
 	var pos = vaisseau.top;
-  clearInterval(id);
+	missile.display = "block"
+  // clearInterval(id);
   id = setInterval(frame, 10);
 	function frame() {
-    if (pos == document.body.clientHeight) {
-      clearInterval(id);
+    if (missile.top < -1 ) {
 			missile.display = "none";
-			myMove();
+
     } else {
-			missile.display = "block";
+			// missile.display = "block";
 			pos -= 10;
       missile.top = pos;
     }
-  }
+	}
 }
 
 //function qui détecte lorsque l'on appuie sur un bouton
 document.onkeydown = function (event) {
 	console.log(event.keyCode);
-	missile.display = "block";
-	let _keyCode = event.keyCode;
 	if (vaisseau.left >= 0 && vaisseau.left <= document.body.clientWidth - 100){
 		switch (event.keyCode) {
 		case 81: // touche Q
@@ -99,9 +100,9 @@ document.onkeydown = function (event) {
 			break;
 		default:
 	}
-	if (_keyCode == 32){
+	if (event.keyCode == 32){
+		myMove();
 
-		myMove(missile);
 	}
 } else if (vaisseau.left < 0) {
 		vaisseau.left = 0;
