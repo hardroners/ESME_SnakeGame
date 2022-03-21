13509791e5ca118c2701abcdd224a4196f04b46c
hardroners <hardroners@gmail.com>
2022-03-21 16:04:19 +0100

    add hitbox

--- scripts/jeu.js
+++ scripts/jeu.js
@@ -7,8 +7,6 @@ function Sprite(filename, abs,ord,size){
 	this._node.style.height=size + "px";
 	this._node.style.width="auto";
 	console.log(filename + "    "+abs+"  "+ ord);
-	// this._node.style.left=abs+"px";
-	// this._node.style.top	=ord+"px";
 	document.body.appendChild(this._node);
 
 //implémentation des sprties en dynamique
@@ -52,14 +50,39 @@ function Sprite(filename, abs,ord,size){
 
 
 //positionnement des sprites
-let alien1 = new Sprite("images/invader.png",3*document.body.clientWidth/8, document.body.clientHeight/4,100);
-let alien2 = new Sprite("images/invader.png",4*document.body.clientWidth/8, document.body.clientHeight/4,100);
-let alien3 = new Sprite("images/invader.png",5*document.body.clientWidth/8, document.body.clientHeight/4,100);
-let vaisseau = new Sprite("images/vaisseau.png", document.body.clientWidth/2, 3*document.body.clientHeight/4,100);
+let alien1 = new Sprite("images/invader.png",2*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien2 = new Sprite("images/invader.png",3*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien3 = new Sprite("images/invader.png",4*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien4 = new Sprite("images/invader.png",5*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien5 = new Sprite("images/invader.png",6*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien6 = new Sprite("images/invader.png",7*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien7 = new Sprite("images/invader.png",8*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien8 = new Sprite("images/invader.png",9*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien9 = new Sprite("images/invader.png",10*document.body.clientWidth/16, document.body.clientHeight/4,60);
+let alien10 = new Sprite("images/invader.png",2*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien11 = new Sprite("images/invader.png",3*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien12 = new Sprite("images/invader.png",4*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien13 = new Sprite("images/invader.png",5*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien14 = new Sprite("images/invader.png",6*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien15 = new Sprite("images/invader.png",7*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien16 = new Sprite("images/invader.png",8*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+let alien17 = new Sprite("images/invader.png",9*document.body.clientWidth/16, 2*document.body.clientHeight/4,60);
+
+let vaisseau = new Sprite("images/vaisseau.png", document.body.clientWidth/2, 3*document.body.clientHeight/4,60);
 // let missile = new Sprite("images/laser.png", vaisseau.left,vaisseau.top,50);
 // missile.display = "none";
-let i = 0;
 
+
+function getRandomInt(min, max) {
+	return Math.floor( Math.random() * (max - min) + min );
+}
+
+
+function createAlien(){
+	let ramdompos = getRandomInt(1,16);
+	let alien = new Sprite("images/invader.png",ramdompos*document.body.clientWidth/16, document.body.clientHeight/4,60);
+	return alien;
+}
 var id = null;
 function myMove() {
 	let missile = new Sprite("images/laser.png", vaisseau.left,vaisseau.top,50);
@@ -72,13 +95,19 @@ function myMove() {
 	function frame() {
     if (missile.top < -1 ) {
 			missile.display = "none";
-
-    } else {
+    } else if (missile.checkCollision(alien)) {
+    	alien.display = "none";
+    }
+     else {
 			// missile.display = "block";
 			pos -= 10;
       missile.top = pos;
     }
 	}
+}
+
+Sprite.prototype.checkCollision = function (other){
+	return (( this.top + this._node.height > other.top) && (this.top<(other.top+other._node.height)) && (this.left+this._node.width>other.left) && (this.left<other.left+other._node.width))
 }
 
 //function qui détecte lorsque l'on appuie sur un bouton
@@ -87,16 +116,16 @@ document.onkeydown = function (event) {
 	if (vaisseau.left >= 0 && vaisseau.left <= document.body.clientWidth - 100){
 		switch (event.keyCode) {
 		case 81: // touche Q
-			vaisseau.left -= 10;
+			vaisseau.left -= 20;
 			break;
 		case 37: // fleche de gauche
-			vaisseau.left -= 10;
+			vaisseau.left -= 20;
 			break;
 		case 68: // touche D
-			vaisseau.left += 10;
+			vaisseau.left += 20;
 			break;
 		case 39: // fleche de droite
-			vaisseau.left += 10;
+			vaisseau.left += 20;
 			break;
 		default:
 	}
